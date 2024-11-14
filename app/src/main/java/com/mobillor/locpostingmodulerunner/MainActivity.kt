0391d0fc4714ca69d7bb561af3f7d7e37ba1a592
Dev-Subh <subharanjan.nayak@mobillor.net>
2024-11-14 11:04:01 +0530

    v2.1

--- app/src/main/java/com/mobillor/locpostingmodulerunner/MainActivity.kt
+++ app/src/main/java/com/mobillor/locpostingmodulerunner/MainActivity.kt
@@ -19,11 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val i = Intent(this, LocPostingStarterActivity::class.java)
-        i.putExtra("baseURL","https://swms.mobillor.net/api/api/api/swms/")
-        i.putExtra("userId","ca5c8f1b-5f19-4fe9-851d-f7950a1f9169")
-        i.putExtra("userToken","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Im5hbWUiOiJNYWRodSAoUGxhbm5lcikiLCJlbWFpbCI6Im1hZGh1c3VkaGFuLmxAbW9iaWxsb3IubmV0Iiwicm9sZUlkIjoiMjNmZDlmNjEtNTRlNS00ZTRmLWI4NWMtMWRhZGZkYzhmOGVlIiwidXVpZCI6Ijc0NTY1NmZiMmNjY2I2YTQifSwiaWF0IjoxNzMwOTc5ODcyLCJleHAiOjE3MzEwMjMwNzJ9.lPmJAz4J451CnGGIat0T72zSxFXhUoTK67L8imHhucA")
-        startActivity(i)
+
         setContent {
             LocPostingModuleRunnerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
